f0da5766d20035dcbcf6d315b07de249f6bd6e1a
Sevenlive <lukaslange@live.de>
2025-02-13 19:49:30 +0100

    Add basic_config.yml

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,3 +4,4 @@ apt install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install ansible -y
 ansible-galaxy install -r requirements.yml
+ansible-playbook basic_config.yml
